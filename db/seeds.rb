bed80b4f55a0c9914deba107109224656c2fd7bc
mnarasimhan <maheshya@gmail.com>
2016-05-09 17:41:21 +0000

    updated seeds.rb for default load

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!")
+Product.create(:name => "Razor 62042", :description => "High Roller BMX/Freestyle Bike", :image_url => "bmx.jpg", :colour => "Black/Blue")
+Product.create(:name => "Wayfarer Hybrid Bike", :description => "Schwinn Women's Wayfarer Hybrid Bike", :image_url => "wayfarer.jpg", :colour => "Black/Blue")
+Product.create(:name => "Mountain Bike", :description => "Extra Sturdy Outdoors Exercise Men's Bicycle 18 Speed Durable Mountain Bike Men", :image_url => "mountain1.jpg", :colour => "Black/Blue")
+Product.create(:name => "X-Games", :description => "FS20 Freestyle Bicycle", :image_url => "bmx1.jpg", :colour => "Black/Blue")

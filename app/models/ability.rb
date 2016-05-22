388e5095eee15377397e9e856b9b3a0d1fc6ef5b
mnarasimhan <maheshya@gmail.com>
2016-05-22 18:09:33 +0000

    deletion comments functionality

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,7 +1,12 @@
 class Ability
   include CanCan::Ability
-    def initialize(user)
-        user ||= User.new # guest user (not logged in)
-        can :manage, User, id: user.id
-      end
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+    end
+  end
 end

d47641aa56a9e0ed2d1d9cee249bf370d3b9e1ba
mnarasimhan <maheshya@gmail.com>
2016-06-03 00:35:09 +0000

    added price and stripe payment module

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522173928) do
+ActiveRecord::Schema.define(version: 20160602235848) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160522173928) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
+    t.decimal  "price"
   end
 
   create_table "users", force: :cascade do |t|

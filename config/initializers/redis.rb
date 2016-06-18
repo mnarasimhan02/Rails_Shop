1dcd6bbca7dbea4a06cb2357af259846d86d4df3
mnarasimhan <maheshya@gmail.com>
2016-06-18 17:26:33 +0000

    updated styles, broken links

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-if Rails.env == 'production'
-$redis = Redis.new(url: ENV["REDIS_URL"])
-else
+
 $redis = Redis.new(:host => 'localhost', :port => 6379)
-end
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 

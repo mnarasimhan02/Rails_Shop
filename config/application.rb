348808f07285f644b42fe66010e33cc41362056e
mnarasimhan <maheshya@gmail.com>
2016-06-18 17:39:58 +0000

    updated styles, broken links

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
-config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
+#config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
